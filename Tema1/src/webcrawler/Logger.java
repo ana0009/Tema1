23634d9547f5f40e446de6837fd3b2cd9f8369c0
emi-dumitru-selab <emi.dumitru@mta.ro>
2020-12-13 15:15:22 +0200

    Am adaugat schelet-Clasa-Logger
    Signed-off-by: emi-dumitru-selab <emi.dumitru@mta.ro>

--- Tema1/src/webcrawler/Logger.java
+++ Tema1/src/webcrawler/Logger.java
@@ -1,6 +1,23 @@
 package webcrawler;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+
 public class Logger {
+    String m_mesaj;
+    String m_path;
+    FileHandler fh;
+    java.util.logging.Logger logger = java.util.logging.Logger.getLogger("MyLog");
+
+
+    public Logger(String m_path) {
+    }
+
+
+    public void logger(String m_mesaj) {
+
+    }
 
     public void print() {
 
