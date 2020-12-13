a5cd467c7fdb808c222d7098ace081c8a0b0076d
emi-dumitru-selab <emi.dumitru@mta.ro>
2020-12-13 15:03:09 +0200

    Am adaugat schelet-Clasa-SearchWord
    Signed-off-by: emi-dumitru-selab <emi.dumitru@mta.ro>

--- Tema1/src/webcrawler/Search.java
+++ Tema1/src/webcrawler/Search.java
@@ -1,8 +1,33 @@
 package webcrawler;
 
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.IOException;
+
+
+
+
 public class Search {
 
-    public void print() {
+    private String m_path;
+    Logger logger = new Logger("Path");
 
+
+    public Search() {
+    }
+
+
+
+    public void search(String m_path) throws IOException {
+    }
+
+
+    public void print() {
     }
 }
