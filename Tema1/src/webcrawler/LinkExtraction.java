229223a2f819a021ddaa92d21f56f6457926438a
Biancaasan <bianca.asan009@yahoo.com>
2020-12-18 17:12:26 +0200

    Implement SearchURL class
    Isue:#2
    The class LinkExtraction extract the existing link from the downloaded
    pages and add the links in queue if they aren't already added.
    
    Signed-off-by: Biancaasan <bianca.asan009@yahoo.com>

--- Tema1/src/webcrawler/LinkExtraction.java
+++ Tema1/src/webcrawler/LinkExtraction.java
@@ -1,54 +1,44 @@
-package crawler;
+package webcrawler;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
-* The class LinkExtraction extract the existing link from the downloaded
-* pages and add the links in queue if they aren't already added.
-*
-*/
+ * The class LinkExtraction extract the existing link from the downloaded
+ * pages and add the links in queue if they aren't already added.
+ *
+ */
 
 public class LinkExtraction {
-    private Matcher mTag, mLink;
-    private Pattern pTag, pLink;
-
     /* get the queue*/
     ProcessURL p = new ProcessURL();
     public Queue<String> q = p.getURL();
 
-    public Queue<String> getQueue(){
+    public LinkExtraction() throws IOException {
+    }
+
+    public Queue<String> getQueue() {
         return q;
     }
 
-
-    private static final String HTML_TAG_PATTERN = "(?i)<a([^>]+)>(.+?)</a>";
-    private static final String HTML_HREF_TAG_PATTERN = "\\s*(?i)href\\s*=\\s*(\"([^\"]*\")|'[^']*'|([^'\">\\s]+))";
-
-    public LinkExtraction() throws IOException {
-        pTag = Pattern.compile(HTML_TAG_PATTERN);
-        pLink = Pattern.compile(HTML_HREF_TAG_PATTERN);
-    }
-
-    public void extractHTMLLinks(String first) {
-        mTag = pTag.matcher(first);
-
-        while (mTag.find()) {
-            String href = mTag.group(1);     /* get the values of href*/
-
-            mLink = pLink.matcher(href);
-
-            while (mLink.find()) {
-                String link = mLink.group(1);
-                if(link != null){
-                    System.out.println(link);
-                }else {
-                    System.out.println("Nu e");
-                }
-                q.add(link);
+    BufferedReader read = new BufferedReader(new FileReader("C:/Users/Bianca/Desktop/Proiect/Downloads/Downloads.html"));
+//pe asta trebuie sa o fac sa ia ca parametru ce mi-a returnat Vicentiu !!!!
+    
+    public Queue<String> extractUrl( ) throws IOException {
+        String line;
+        while((line = read.readLine()) != null){
+            String regex = "(https|http?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(line);
+            while (m.find()) {
+                q.add(m.group());
+                q.add("\n");
             }
         }
+        return q;
     }
 }
